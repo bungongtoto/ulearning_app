f47627c51bed0eed1330831c78a657304883566c
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2023-10-02 22:03:07 +0100

    initialise application home screen

--- lib/pages/application/application.dart
+++ lib/pages/application/application.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:ulearning_app/common/widgets/app_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/common/utils/app_colors.dart';
+import 'package:ulearning_app/common/widgets/app_shadows.dart';
+import 'package:ulearning_app/pages/application/widgets/widgets.dart';
+import 'dart:developer' as developer;
 
 class Application extends StatefulWidget {
   const Application({super.key});
@@ -9,10 +13,36 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppbar(title: "Home page"),
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Container(),
+          bottomNavigationBar: Container(
+            width: 392.h,
+            height: 55.h,
+            decoration: appBoxShadowWithRadius(),
+            child: BottomNavigationBar(
+              currentIndex: index,
+              onTap: (value) {
+                setState(() {
+                  index = value;
+                });
+                developer.log(value.toString());
+              },
+              selectedItemColor: AppColors.primaryElement,
+              elevation: 0,
+              items: bottomTabs,
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              unselectedItemColor: AppColors.primaryFourElementText,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
